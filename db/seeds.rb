59186814111434bd7a3ece068f701bb9565b226c
Jared Cosulich <jared@irrationaldesign.com>
2015-01-02 13:17:59 -0500

    css tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,8 @@ Profile.create(
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   title: 'Founder & CEO',
   industry: 'Technology',
-  company: 'FamilyID, Inc.'
+  company: 'FamilyID, Inc.',
+  website: 'http://www.familyid.com',
+  twitter: 'https://twitter.com/rochellenemrow',
+  linkedin: 'https://www.linkedin.com/pub/rochelle-nemrow/5/35a/426'
 )
