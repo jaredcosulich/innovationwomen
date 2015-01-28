3e72b7de1e99c84e006a1d2e908339813b738d02
Jared Cosulich <jared@irrationaldesign.com>
2015-01-27 21:04:59 -0500

    coming soon routes, about page, working on images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,8 @@ class WelcomeController < ApplicationController
   def index
     @featured_members = Profile.all * 8
   end
+  
+  def coming_soon
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Innovationwomen::Application.routes.draw do
   resources :profiles
 
   root to: 'welcome#index'
+  get 'about', to: 'welcome#about'
   get 'search', to: 'profiles#search'
+  get 'find_profile', to: 'welcome#coming_soon'
+  get 'new_event', to: 'welcome#coming_soon'
+  get 'events', to: 'welcome#coming_soon'
+  get 'organizations', to: 'welcome#coming_soon'
 end
