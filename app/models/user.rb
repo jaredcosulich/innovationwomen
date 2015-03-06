7ff11be7b277ca5a6e29857ad05e9be3a6492218
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-06 15:12:28 -0500

    Validate that email and password cannot be null

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  
+
   has_one :profile
-  
-  validates :email, presence: true
-  validates :password, presence: true
-  
-  
-  
+
+  validates :email, presence: true, null: false
+  validates :password, presence: true, null: false
 end
