70c32800ef45c11b5e28ff1081a700504322c40a
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-10 21:06:17 -0400

    Use a regular expression to validate the format of an email
    The regular expression only looks for an '@' and one '.'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  VALID_EMAIL = /.+@.+\..+/i
+
   has_one :profile, dependent: :destroy
 
+  validates_format_of :email,       with: VALID_EMAIL
   validates :email,                 presence: true, null: false, uniqueness: true
   validates :password,              presence: true, null: false,
                                     confirmation: true, length: { minimum: 6 }
