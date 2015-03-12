f198072d57bef9611990fa17b5a1d37f0545ff7f
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-11 20:15:11 -0400

    Move the secret token into an ENV variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Innovationwomen::Application.config.secret_key_base = '60460fc61208a9e1a161964f1fb34bbda43c6ffccb2e0447f1a7857300bafa41f4738ef1cb13577d63ce284057dfe0db1644a682019327fb05e3117dabb174b7'
+Innovationwomen::Application.config.secret_key_base = ENV['SECRET_TOKEN']
