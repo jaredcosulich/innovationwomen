2ea43ddf477cafb8455bd14c77c209dbea10ffc8
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-16 16:58:18 -0400

    Add a comma after the greeting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def reset_password_email(user)
     @user = user
     @url = edit_password_reset_url(user.reset_password_token)
-    @greeting = "Hi #{user.profile.name}"
+    @greeting = "Hi #{user.profile.name},"
 
     recipient = user.email
     subject = 'Reset your password for Innovation Women'
