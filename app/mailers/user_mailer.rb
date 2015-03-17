6117f9e83edb40b73749a84eadce6a9c7ff50172
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-17 15:34:50 -0400

    Add a 'no-reply' prefex to the default from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
-  default from: "#{ActionMailer::Base.default_url_options[:host]}"
+  default from: "no-reply@#{ActionMailer::Base.default_url_options[:host]}"
 
   def reset_password_email(user)
     @user = user
     @url = edit_password_reset_url(user.reset_password_token)
     @greeting = "Hi #{user.profile.name},"
 
-    recipient = user.email
+    recipient = @user.email
     subject = 'Reset your password for Innovation Women'
 
     mail(to: recipient, subject: subject)
