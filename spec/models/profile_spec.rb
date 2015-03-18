2e94d6f8d1f7b2a249e031d834048e30a4218acc
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-18 14:50:58 -0400

    Test the validations and relationships in the profile model

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
-describe Profile do
+RSpec.describe Profile, type: :model do
+  it { should belong_to(:user) }
+  it { should have_many(:topics) }
+
+  it { should validate_presence_of(:name) }
+  it { should_not allow_value('', nil).for(:name) }
+
+  it { should accept_nested_attributes_for(:user) }
 end
