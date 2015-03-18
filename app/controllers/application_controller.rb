fd829882a1c48bed68b6b7b2087827a80ce3d2d7
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-18 16:38:10 -0400

    Start the integration of CanCan for mult-role management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to :back
+    flash.now[:alert] = 'You do not have access to this page.'
+  end
 end
