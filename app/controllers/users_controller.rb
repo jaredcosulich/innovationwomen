5da0211ba200674fac750f43ccc5eaffe7e9ada9
Sherry Seibel <sherry.seibel@gmail.com>
2015-03-29 14:46:35 +0200

    Create a separate new user sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.xml  { render xml: @users }
-    end
   end
 
   def show
@@ -19,10 +15,6 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.xml  { render xml: @user }
-    end
   end
 
   def edit
@@ -32,14 +24,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to :users }
-        format.xml { render xml: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.xml { render xml: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      login(params[:user][:email], params[:user][:password])
+      redirect_to root_path
+    else
+      render action: 'new'
     end
   end
 
@@ -71,8 +60,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
+      :first_name,
+      :last_name,
       :email,
       :password,
+      :password_confirmation,
     )
   end
 end
