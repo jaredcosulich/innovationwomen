e92516fcddd1612d0de5735e1d735a089a5ca2a2
Sherry Seibel <sherry.seibel@gmail.com>
2015-04-07 11:45:04 -0400

    Change the smtp settings to send from localhost:1025

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,19 +37,10 @@ Innovationwomen::Application.configure do
   }
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { address: 'localhost', port: '1025' }
+  config.action_mailer.default_url_options = { address: 'localhost', port: 5000 }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'example.com',
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: ENV['EMAIL_USERNAME'],
-    password: ENV['EMAIL_PASSWORD']
-  }
 end
