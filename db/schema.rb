deb329ec19da8ae9556a14e10ef44fa9a4a76776
Sherry Seibel <sherry.seibel@gmail.com>
2015-04-09 11:37:32 -0400

    Update the schema
    The schema contained database information form other branches.
    It has been updated to only contain relevant information to master.

--- db/schema.rb
+++ db/schema.rb
@@ -44,17 +44,6 @@ ActiveRecord::Schema.define(version: 20150409004922) do
     t.datetime "picture_updated_at"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "resource_id"
-    t.string   "resource_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
-
   create_table "topics", force: :cascade do |t|
     t.integer  "profile_id"
     t.string   "title"
@@ -83,12 +72,5 @@ ActiveRecord::Schema.define(version: 20150409004922) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
-
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-  end
-
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
 end
